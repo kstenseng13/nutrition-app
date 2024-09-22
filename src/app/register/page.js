5b3e6e82af0933875958535a97553f8e469632f1
Kailee Stenseng <e-bird14@hotmail.com>
2024-09-21 22:10:07 -0500

    started adding stuff. WIP.

---
+++ src/app/register/page.js
@@ -0,0 +1,14 @@
+import CreateAccount from '../components/createAccount';
+
+export default function Register() {
+    return (
+<div className="text-center h-screen bg-cover bg-[radial-gradient(circle,rgba(31,41,55,0.9),rgba(31,41,55,0.4)),url('https://cdn.pixabay.com/photo/2017/01/09/11/30/dumbbell-1966247_1280.jpg')] flex items-center justify-center">        
+<div className='bg-beige w-1/3 p-8 rounded-lg shadow-lg'>
+            <h1 className='text-3xl my-8 font-semibold'>Join Us</h1>
+            <div className='flex items-center justify-center'>
+                <CreateAccount />
+            </div>
+        </div>
+    </div>
+    );
+};
