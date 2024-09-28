e1a2d2544e0d0f8e1d11f37da5e88bae4e9fe2ce
Shane <flyinfast8@yahoo.com>
2024-09-28 14:00:44 -0600

    Logout functionality
    Tried to fix the logout feature and get it working

--- src/app/components/Logout.js
+++ src/app/components/Logout.js
@@ -18,7 +18,12 @@ const Logout = () => {
   }, [navigate]);
 
   return (
-    <div>
+    
+    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+            <h2>Are you sure you want to Logout?</h2>
+            <br></br>
+            <Link href='/login'><button className="btn-primary" onClick={localStorage.removeItem('userToken')}>Logout</button></Link>
+       
       <h1>You have been logged out.</h1>
       <p>Redirecting to the login page...</p>
     </div>
--- src/app/logout/page.js
+++ src/app/logout/page.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Logout from '../components/logout';
 //import Logout from '../components/Logout';
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
         <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
             <h2>Are you sure you want to Logout?</h2>
             <br></br>
-            <Link href='/login'><button className="btn-primary">Logout</button></Link>
+            <Link href='/Logout'><button className="btn-primary">Logout</button></Link>
         </div>
 
         
