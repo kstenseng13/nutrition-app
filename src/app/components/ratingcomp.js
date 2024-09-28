cdfad45ced873eb550e89d79393a3d59a4ee0e38
stlbpw <69367908+stlbpw@users.noreply.github.com>
2024-09-28 14:01:35 -0500

    TW CSS update

--- src/app/components/ratingcomp.js
+++ src/app/components/ratingcomp.js
@@ -5,7 +5,7 @@ import React from 'react';
 const RatingComp = ({ color, text }) => {
 
     return (
-        <div style={{ backgroundColor: color, width: '300px', height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid #ccc', borderRadius: '8px', marginTop: '20px' }}>
+        <div className="w-[300px] h-[100px] flex items-center justify-center border border-gray-300 rounded-lg mt-5" style={{ backgroundColor: color }}>
             <p>{text}</p>
         </div>
     );
--- src/app/test/colorbuttons.js
+++ src/app/test/colorbuttons.js
@@ -1,12 +1,13 @@
 // ColorButtons.js
 import React from 'react';
 
+
 const ColorButtons = ({ changeColorAndText }) => {
     return (
         <div style={{ marginTop: '20px' }}>
             <button className="btn-primary" onClick={() => changeColorAndText('red', 'POOR')}>Red</button>
-            <button className="btn-primary" onClick={() => changeColorAndText('yellow', 'USE CAUTION')} style={{ marginLeft: '10px' }}>Yellow</button>
-            <button className="btn-primary" onClick={() => changeColorAndText('green', 'GOOD')} style={{ marginLeft: '10px' }}>Green</button>
+            <button className="btn-primary ml-2" onClick={() => changeColorAndText('yellow', 'USE CAUTION')}>Yellow</button>
+            <button className="btn-primary ms-2" onClick={() => changeColorAndText('green', 'GOOD')}>Green</button>
         </div>
     );
 };
